cf630c3bf5f4b5f45c7295909d87d1cf6743f0a9
Larry Price <laprice@gmail.com>
2018-11-06 15:38:43 -0800

    applying futurize

--- stylometry/download.py
+++ stylometry/download.py
@@ -1,6 +1,10 @@
+from __future__ import print_function
+from future import standard_library
+standard_library.install_aliases()
 import os
 import csv
-from urllib2 import urlopen, URLError, HTTPError
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
 
 def gutenberg_mirror_download(row):
     try:
@@ -11,16 +15,16 @@ def gutenberg_mirror_download(row):
         url = "http://mirrors.xmission.com/gutenberg/" + path
         # url = "http://www.gutenberg.org/ebooks/%d.txt.utf-8" % int(row['gutenberg_id'])
         f = urlopen(url)
-        print "downloading " + url
+        print("downloading " + url)
         if not os.path.exists(row['author']):
             os.makedirs(row['author'])
         with open(row['author'] + '/' + row['title'] + '.txt', "wb") as local_file:
             local_file.write(f.read())
 
-    except HTTPError, e:
-        print "HTTP Error:", e.code, url
-    except URLError, e:
-        print "URL Error:", e.reason, url
+    except HTTPError as e:
+        print("HTTP Error:", e.code, url)
+    except URLError as e:
+        print("URL Error:", e.reason, url)
 
 def gutenberg():
     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'gutenberg_mirror.csv'),'rb') as csvfile:
